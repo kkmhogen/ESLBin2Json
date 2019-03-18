6c9db16122e6e41517f33994a779d1c6415d9f6e
hogen <hogen@kkmcn.com>
2019-03-18 17:53:00 +0800

    add readme file

--- Bin2Json/ToolsBytes.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-uint8_t tools_char2Bytes(char cValue1, char cValue2);
-
-uint8_t tools_itoa(int32_t val, char* dst, int32_t radix);
-
-int tools_byteToHexStrs(const uint8_t* source, char* dest, uint32_t sourceLen);
